02996482d242c4439b40034bcb0b9ce6ccb0d9db
Elaina-Alex <luanyin4444@qq.com>
2022-04-13 13:04:46 +0800

    ~

--- main.py
+++ main.py
@@ -14,13 +14,21 @@ def update():
         download_test = True
     data = json.loads(open('update.json', 'r').read())
     if data['version'] < response['version']:
-        print("检测到有新版本", response['version'], "尝试自动更新")
-        download_test = True
-    if download_test or not os.path.exists(data['name'] + ".exe"):
+        print("检测到有新版本", response['version'], "是否进行更新？[yes/no]")
+        choice = PixivAPI.input_('>').strip()
+        if choice == "yes":
+            download_test = True
+            print("开始更新", response['version'], "版本")
+        else:
+            download_test = False
+
+    if download_test:
         with open(data['name'] + ".exe", 'wb') as file:
-            file.write(PixivAPI.HttpUtil.get(response['download_url'].format(response['version'],)).content)
+            print(response['download_url'].format(response['version']))
+            file.write(PixivAPI.HttpUtil.get(response['download_url'].format(response['version'])).content)
         print(data['name'] + ".exe", "下载完毕")
         json.dump(response, open('update.json', 'w'))
+        print("三秒后自动退出脚本...")
         sys.exit()
 
 
@@ -188,6 +196,6 @@ def shell():
 
 if __name__ == '__main__':
     set_config()
-    shell_pixiv_token()
     update()
+    shell_pixiv_token()
     shell()
