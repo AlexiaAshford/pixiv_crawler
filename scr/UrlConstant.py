0619111cb80bff0cf5758fc355e6c7a4c3f535c1
Alex <luanyin4444@qq.com>
2022-06-28 11:13:49 +0800

    ~

---
+++ scr/UrlConstant.py
@@ -0,0 +1,13 @@
+IMAGE_INFORMATION = "https://api.obfs.dev/api/pixiv/illust"
+
+# PIXIV_APP_API
+FOLLOWING_INFORMATION = "user/following"
+BOOKMARK_INFORMATION = "user/bookmarks/illust"
+RECOMMENDED_INFORMATION = "illust/recommended"
+AUTHOR_INFORMATION = "user/illusts"
+ACCOUNT_INFORMATION = "user/detail"
+RANKING_INFORMATION = "illust/ranking"
+SEARCH_INFORMATION = "search/illust"
+
+PIXIV_HOST = "https://app-api.pixiv.net/v1/"
+
