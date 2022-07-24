7bf8ce0fd4b6591d792f095248de58faed430e0e
VeronicaAlexia <luanyin4444@qq.com>
2022-07-24 11:51:37 +0800

    ~

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='pixivlib',
-    version='1.2',
+    name='pixiv',
+    version='1.4',
     packages=['src', 'src.PixivUtil', 'tools'],
     include_package_data=True,  # 自动打包文件夹内所有数据
     url='https://github.com/VeronicaAlexia/pixiv_crawler',
     license='MIT License',
     author='Alex',
     author_email='',
-    entry_points={"console_scripts": ["pixivlib = src.__main__:main"]},
+    entry_points={"console_scripts": ["pixiv = src.__main__:main"]},
     description='download pixiv illustrations',
     zip_safe=True
 )
