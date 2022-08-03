3495fc3d4c4b1d18ea283238778352efdfa1ad9e
VeronicaAlexia <luanyin4444@qq.com>
2022-08-03 21:39:15 +0800

    ~

--- tools/instance.py
+++ tools/instance.py
@@ -8,13 +8,13 @@ class Msg:
         "输入首字母",
         "h | help\t\t\t\t\t\t--- 显示说明",
         "q | quit\t\t\t\t\t\t--- 退出正在运作的程序",
-        "d | picture\t\t\t\t\t\t--- 输入id或url下载插画",
+        "d | picture\t\t\t\t\t--- 输入id或url下载插画",
         "t | recommend\t\t\t\t\t--- 下载pixiv推荐插画",
         "s | start\t\t\t\t\t\t--- 下载账号收藏插画",
         "r | rank\t\t\t\t\t\t--- 下载排行榜作品",
         "n | tag name\t\t\t\t\t--- 输入插画名或者表情名",
         "u | read text pid\t\t\t\t\t--- 读取本地文本里的pid批量下载",
-        "f | follow\t\t\t\t\t\t--- 下载关注的画师作品",
+        "f | follow\t\t\t\t\t--- 下载关注的画师作品",
     ]
 
 
