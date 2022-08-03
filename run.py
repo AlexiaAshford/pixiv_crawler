f11b4162a731f31f5b4a4733fd6b63c1c233658e
VeronicaAlexia <luanyin4444@qq.com>
2022-08-03 18:50:07 +0800

    ~

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 from tools.instance import *
-from src import __main__, shell_test_pixiv_token
+from src import main, shell_test_pixiv_token
 
 if __name__ == '__main__':
     try:
-        __main__.set_update_config()
+        main.set_update_config()
         shell_test_pixiv_token()
-        __main__.shell_parser(__main__.start_parser())
+        main.shell_parser(main.start_parser())
     except KeyboardInterrupt:
         print("已手动退出程序")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pixiv',
-    version='1.4',
+    version='1.6.0',
     packages=['src', 'src.PixivUtil', 'tools'],
     include_package_data=True,  # 自动打包文件夹内所有数据
     url='https://github.com/VeronicaAlexia/pixiv_crawler',
@@ -10,7 +10,7 @@ setup(
     author='Alex',
     author_email='',
     entry_points={
-        "console_scripts": ["pixiv = src.__main__:main"]
+        "console_scripts": ["pixiv = src.main:main"]
     },
     description='download pixiv illustrations',
     zip_safe=True
